2491fd2aee1b93ae6d7ec02ccc2f9eb38cd8b980
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-10-20 00:25:14 -0700

    confirm box

---
+++ components/NumberContainer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import Color from '../constants/color'
+
+export default function NumberContainer(props) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.number}>{props.children}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        borderWidth:2,
+        borderColor:Color.accent,
+        padding:10,
+        borderRadius:10,
+        marginVertical:10,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    number:{
+        color:Color.accent,
+        fontSize:22
+    }
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,23 +1,71 @@
-import React from 'react'
-import { StyleSheet, Text, View ,TextInput,Button } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View ,TextInput,Button,TouchableWithoutFeedback,Keyboard,Alert} from 'react-native'
 
 import Card from '../components/Card'
 import Colors from '../constants/color'
 import Input from '../components/Input'
+import NumberContainer from '../components/NumberContainer'
 
 export default function StartGameScreen() {
+
+    const [enteredValue, setenteredValue] = useState('')
+    const [confirmed, setconfirmed] = useState(false)
+    const [selectedNumber, setselectedNumber] = useState()
+
+    const numberInputHandler = inputText => {
+        setenteredValue(inputText)
+    }
+
+    const resetInputhandler = () =>{
+        setenteredValue('')
+        setconfirmed(false)
+
+    }
+
+    const confirmInputHandler = () =>{
+
+        const chosenNumber = parseInt(enteredValue);
+        if(isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99){
+            Alert.alert('Invalid number!','Number has to be a number between 1 and 99',[{text:'Okay',style:'destructive',onPress:resetInputhandler}])
+            return;
+        }
+        setconfirmed(true)
+        setenteredValue('')
+        setselectedNumber(parseInt(enteredValue))
+
+        Keyboard.dismiss()
+
+    }
+
+    let confirmedOutput;
+
+    if(confirmed){
+        confirmedOutput = (
+            <Card style={styles.summaryContainer}>
+                <Text>You Selected</Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title="START BUTTON" />
+            </Card>
+        )
+    }
+
     return (
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss()
+        }}>
         <View style={styles.screen}>
             <Text style={styles.title}>The Game Screen!</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <Input style={styles.input} blurOnSubmit={true} autoCapitalize='none' autoCorrect={false} keyboardType="number-pad" maxLength={2} />
+                <Input style={styles.input} blurOnSubmit={true} autoCapitalize='none' autoCorrect={false} keyboardType="number-pad" maxLength={2} onChangeText={numberInputHandler} value={enteredValue} />
                 <View style={styles.buttonContainer}>
-                    <View style={styles.button}><Button color={Colors.accent} title="Reset" onPress={() => {}} /></View>
-                    <View style={styles.button}><Button color={Colors.primary} title="Confirm" onPress={() => {}} /></View>
+                    <View style={styles.button}><Button color={Colors.accent} title="Reset" onPress={resetInputhandler} /></View>
+                    <View style={styles.button}><Button color={Colors.primary} title="Confirm" onPress={confirmInputHandler} /></View>
                 </View>
+                {confirmedOutput}
             </Card>
         </View>
+        </TouchableWithoutFeedback>
     )
 }
 
@@ -48,5 +96,9 @@ const styles = StyleSheet.create({
     input:{
         width:50,
         textAlign:'center'
+    },
+    summaryContainer:{
+        marginTop:20,
+        alignItems:'center'
     }
 })
